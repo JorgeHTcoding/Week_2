8a4058b0ab949f5146da7a582af026e1db0b7497
JorgeHTcoding <jht.coding@gmail.com>
2022-05-10 17:59:56 +0200

    Subida retos 1-6 dia 7

---
+++ POO2/person.js
@@ -0,0 +1,38 @@
+class Person {
+    constructor (nombre,edad,altura,peso,colorPelo,yearOfBirth,hobbies){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.altura = altura;
+        this.peso = peso;
+        this.colorPelo = colorPelo;
+        this.yearOfBirth = yearOfBirth;
+        this.hobbies = hobbies;
+            }
+calcularImc(peso,altura){
+let resultadoImc = (this.peso / (this.altura * this.altura)) * 10000
+console.log(resultadoImc)
+}
+
+calcularEdad(yearOfBirth){
+let anyo = new Date().getFullYear();
+let resultadoYoB = anyo - this.yearOfBirth; 
+console.log(resultadoYoB)
+}
+// si lo conviertes a una función para concatenar la variable nueva a través de un parámetro ya que no conocemos ninguna manera de darle un valor global a la variable desde este punto
+// printAll(persona){
+//     let print = Object.keys(persona1)
+//     print.forEach((valor) => { console.log(valor + " - " + persona1[valor]
+//     );})
+
+printAll()  {
+       
+   console.log("Nombre" + " - " + this.nombre + " | " + " Edad "  + " - " + this.edad  + " | " +"Altura" + " - " + this.altura + " | " +
+   "Peso" + " - " + this.peso + " | " + " ColorPelo "  + " - " + this.colorPelo  + " | " + "YearOfBirth" + " - " + this.yearOfBirth + " | " + "Hobbies" + " - " + this.hobbies);
+}
+printHobbies(hobbies){
+    console.log(this.hobbies);
+
+}
+}
+
+module.exports.Person = Person;
